4b5002ba2280440118e92b9111308a4ff94f10ff
star-56 <benzendizoniumchloride@gmail.com>
2026-02-25 14:42:52 +0530

    Meet scroll bug resolved

--- components/meetings/MeetingsMainView.tsx
+++ components/meetings/MeetingsMainView.tsx
@@ -435,7 +435,7 @@ export function MeetingsMainView() {
         </div>
 
         {/* Scrollable Content */}
-        <ScrollArea className="flex-1">
+        <ScrollArea className="flex-1 min-h-0">
           <div className="px-6 py-5">
             {/* Stats Grid */}
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-6">
